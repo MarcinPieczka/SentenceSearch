2cbc9f02c22601ad0357f459b849928d7e681b91
Marcin Pieczka <marcin.pieczka@gmail.com>
2019-11-28 00:14:27 +0100

    just for backup purposes

--- collectors/polish_rulings/collect_polish_rullings.py
+++ collectors/polish_rulings/collect_polish_rullings.py
@@ -3,6 +3,8 @@ import argparse
 import requests
 import datetime
 import logging
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
 from bs4 import BeautifulSoup
 from collections import deque
 
@@ -13,12 +15,28 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 def main(collect_all, date_from, date_to):
+    data = {}
     if not any((collect_all, date_from, date_to)):
         print("You have to set at least one parameter")
         return
     search_page_url = prepare_search_page_url(collect_all, date_from, date_to)
-    for url in document_urls_generator(search_page_url):
-        print(url)
+    for url in DocURLGetter(search_page_url):
+        data[url] = {}
+        details_url = url
+        content_url = url.replace('details', 'content')
+        regulations_url = url.replace('details', 'regulations')
+
+        print(details_url, content_url, regulations_url)
+
+
+        with ThreadPoolExecutor(max_workers=3) as executor:
+            futures = [
+                executor.submit(parse_details_page, details_url),
+                executor.submit(parse_content_page, content_url),
+                executor.submit(parse_regulations_page, regulations_url),
+            ]
+            for future in as_completed(futures):
+                data[url].update(future.result())
         
 
 def prepare_search_page_url(collect_all, date_from, date_to):
@@ -29,9 +47,18 @@ def prepare_search_page_url(collect_all, date_from, date_to):
         if not d:
             d = '$N'
     return SEARCH_PAGE_URL_FORMAT.format(date_from=date_from, date_to=date_to) + '{}'
+
+def parse_details_page(url):
+    return {}
+
+def parse_content_page(url):
+    return {}
+
+def parse_regulations_page(url):
+    return {}
         
 
-class document_urls_generator():
+class DocURLGetter():
     def __init__(self, url):
         self.url = url
         self.page = None
@@ -51,10 +78,12 @@ class document_urls_generator():
                     raise StopIteration
                 self.page_nr += 1
                 self.page = BeautifulSoup(response.text, 'html.parser')
-                self.links = list(self.page.find_all('a'))
-                self.links = deque(
-                    filter(lambda l: l.attrs.get('href', '').startswith('/details'), self.links)  # .startswith('/details')
+                self.links = filter(
+                    lambda l: l.attrs.get('href', '').startswith('/details'), 
+                    self.page.find_all('a')
                 )
+                print(self.links)
+                self.links = deque(map(lambda l: l.attrs['href'], self.links))
 
 
 if __name__ == '__main__':
